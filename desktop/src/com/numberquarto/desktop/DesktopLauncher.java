d8a124dc618e3b19461132e4b65ab57016d213e2
theworldbright <sushionigiris@gmail.com>
2014-06-05 18:42:34 +0900

    Initial commit

---
+++ desktop/src/com/numberquarto/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.numberquarto.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.numberquarto.Launch;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Launch(), config);
+	}
+}
