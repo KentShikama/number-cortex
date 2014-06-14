9a04098128ab5d5125b44624fcc8991ee6319a4a
theworldbright <sushionigiris@gmail.com>
2014-06-14 12:48:31 +0900

    Add play screen

---
+++ core/src/com/numberquarto/PlayScreen.java
@@ -0,0 +1,70 @@
+package com.numberquarto;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+
+public class PlayScreen implements Screen {
+	
+	private Game game;
+	private Stage stage;
+	
+	private static final Skin skin = Assets.gameSkin;
+	private static final String RED_BACKGROUND = "red_background";
+	private static final String BLUE_BACKGROUND = "blue_background";
+
+
+	PlayScreen(Game game) {
+		this.game = game;
+	}
+
+	@Override
+	public void render(float delta) {
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		stage.act(delta);
+		stage.draw();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		stage.getViewport().update(width, height, true);
+	}
+
+	@Override
+	public void show() {
+		stage = new Stage(new FitViewport(Launch.SCREEN_WIDTH, Launch.SCREEN_HEIGHT));
+		Gdx.input.setInputProcessor(stage);
+
+		ScreenBackground background = new ScreenBackground(skin, RED_BACKGROUND);
+		stage.addActor(background);
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
