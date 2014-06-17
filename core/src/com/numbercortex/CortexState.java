91eae046f25947ede31ead0770ba37d8acd9cf8f
theworldbright <sushionigiris@gmail.com>
2014-06-17 17:12:32 +0900

    Add bean to hold state for Number Cortex

---
+++ core/src/com/numbercortex/CortexState.java
@@ -0,0 +1,85 @@
+package com.numbercortex;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Map;
+
+public class CortexState implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private ArrayList<String> players;
+	private String currentPlayer;
+	private int chosenNumber;
+	private Map<Integer, Integer> boardMap;
+	private ArrayList<Integer> availableNumbers;
+	private String winner; // Optional
+	private int[] winningCoordinates; // Optional
+
+	private CortexState (CortexStateBuilder builder) {
+		this.players = builder.players;
+		this.currentPlayer = builder.currentPlayer;
+		this.chosenNumber = builder.chosenNumber;
+		this.boardMap = builder.boardMap;
+		this.availableNumbers = builder.availableNumbers;
+		this.winner = builder.winner;
+		this.winningCoordinates = builder.winningCoordinates;
+	}
+	
+	public ArrayList<String> getPlayers() {
+		return players;
+	}
+	
+	public String getCurrentPlayer() {
+		return currentPlayer;
+	}
+
+	public int getChosenNumber() {
+		return chosenNumber;
+	}
+
+	public Map<Integer, Integer> getBoardMap() {
+		return boardMap;
+	}
+	
+	public ArrayList<Integer> getAvailableNumbers() {
+		return availableNumbers;
+	}
+
+	public String getWinner() {
+		return winner;
+	}
+
+	public int[] getWinningCoordinates() {
+		return winningCoordinates;
+	}
+
+	public static class CortexStateBuilder {
+		private final ArrayList<String> players;
+		private final String currentPlayer;
+		private final int chosenNumber;
+		private final Map<Integer, Integer> boardMap;
+		private final ArrayList<Integer> availableNumbers;
+		private String winner; // Optional
+		private int[] winningCoordinates; // Optional
+		
+		public CortexStateBuilder(String currentPlayer, ArrayList<String> players, int chosenNumber, Map<Integer, Integer> boardMap, ArrayList<Integer> availableNumbers) {
+			this.currentPlayer = currentPlayer;
+			this.players = players;
+			this.chosenNumber = chosenNumber;
+			this.boardMap = boardMap;
+			this.availableNumbers = availableNumbers;
+		}
+		
+		public CortexStateBuilder win(String winner, int[] winningCoordinates) {
+			this.winner = winner;
+			this.winningCoordinates = winningCoordinates;
+			return this;
+		}
+		
+		public CortexState build() {
+			return new CortexState(this);
+		}
+	}
+	
+}
