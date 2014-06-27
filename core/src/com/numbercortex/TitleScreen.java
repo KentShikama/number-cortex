7183b6263cc5baa8ef42a200707efd3d94661148
theworldbright <sushionigiris@gmail.com>
2014-06-27 14:03:31 +0900

    Clean up title screen

--- core/src/com/numbercortex/TitleScreen.java
+++ core/src/com/numbercortex/TitleScreen.java
@@ -4,34 +4,35 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
-import com.badlogic.gdx.scenes.scene2d.ui.Button.ButtonStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
-import com.badlogic.gdx.utils.viewport.FitViewport;
 
 public class TitleScreen implements Screen {
-
 	abstract class TitleScreenButton {
 		private Button button;
+
 		TitleScreenButton(String buttonName, int index, ClickListener listener) {
 			Drawable buttonDrawable = skin.getDrawable(buttonName);
 			TextureRegion buttonTexture = skin.getRegion(buttonName);
-			Button.ButtonStyle buttonStyle = new Button.ButtonStyle(buttonDrawable, buttonDrawable, buttonDrawable);
+			Button.ButtonStyle buttonStyle = new Button.ButtonStyle(
+					buttonDrawable, buttonDrawable, buttonDrawable);
 			button = new Button(buttonStyle);
-			button.setBounds(175, Launch.SCREEN_HEIGHT - (756 + index * 80), buttonTexture.getRegionWidth(), buttonTexture.getRegionHeight());
+			button.setBounds(175, Launch.SCREEN_HEIGHT - (756 + index * 80),
+					buttonTexture.getRegionWidth(),
+					buttonTexture.getRegionHeight());
 			button.addListener(listener);
 		}
+
 		public Button getButton() {
 			return button;
 		}
 	}
+
 	class PlayButton extends TitleScreenButton {
 		PlayButton() {
 			super(PLAY_BUTTON, 0, new ClickListener() {
@@ -42,6 +43,7 @@ public class TitleScreen implements Screen {
 			});
 		}
 	}
+
 	class PassAndPlayButton extends TitleScreenButton {
 		PassAndPlayButton() {
 			super(PASS_AND_PLAY_BUTTON, 1, new ClickListener() {
@@ -52,6 +54,7 @@ public class TitleScreen implements Screen {
 			});
 		}
 	}
+
 	class PlayOnlineButton extends TitleScreenButton {
 		PlayOnlineButton() {
 			super(PLAY_ONLINE, 2, new ClickListener() {
@@ -62,6 +65,7 @@ public class TitleScreen implements Screen {
 			});
 		}
 	}
+
 	class TutorialButton extends TitleScreenButton {
 		TutorialButton() {
 			super(TUTORIAL, 3, new ClickListener() {
@@ -72,8 +76,9 @@ public class TitleScreen implements Screen {
 			});
 		}
 	}
+
 	public static final String TAG = TitleScreen.class.getName();
-	
+
 	private static final String TITLE_BACKGROUND = "title_background";
 	private static final String PLAY_BUTTON = "play";
 	private static final String PASS_AND_PLAY_BUTTON = "pass_and_play";
@@ -81,7 +86,6 @@ public class TitleScreen implements Screen {
 	private static final String TUTORIAL = "tutorial";
 
 	private static Skin skin = Assets.homeSkin;
-	
 	private Stage stage;
 	private Game game;
 
@@ -98,7 +102,8 @@ public class TitleScreen implements Screen {
 	}
 
 	private void buildBackground() {
-		ScreenBackground background = new ScreenBackground(skin, TITLE_BACKGROUND);
+		ScreenBackground background = new ScreenBackground(skin,
+				TITLE_BACKGROUND);
 		stage.addActor(background);
 	}
 
@@ -112,7 +117,7 @@ public class TitleScreen implements Screen {
 		stage.addActor(playOnlineButton.getButton());
 		stage.addActor(tutorialButton.getButton());
 	}
-	
+
 	@Override
 	public void render(float delta) {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
@@ -124,12 +129,12 @@ public class TitleScreen implements Screen {
 	public void resize(int width, int height) {
 		stage.getViewport().update(width, height, true);
 	}
-	
+
 	@Override
 	public void resume() {
 		Assets.loadHome();
 	}
-	
+
 	@Override
 	public void dispose() {}
 
@@ -138,5 +143,4 @@ public class TitleScreen implements Screen {
 
 	@Override
 	public void pause() {}
-
 }
