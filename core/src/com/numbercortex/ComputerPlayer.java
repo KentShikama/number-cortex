3df1f2b7dc73f36256054b93d0f43762ecb35862
theworldbright <sushionigiris@gmail.com>
2014-06-27 23:03:05 +0900

    Implement simple AI

--- core/src/com/numbercortex/ComputerPlayer.java
+++ core/src/com/numbercortex/ComputerPlayer.java
@@ -1,13 +1,21 @@
 package com.numbercortex;
 
+import java.util.Map;
+
 public class ComputerPlayer implements Player {
 
 	private String name;
 	private PlayScreen screen;
+	private Exchangeable messenger;
+	
+	private enum Difficulty {
+		EASY, MEDIUM, HARD;
+	}
 
 	public ComputerPlayer(String name, PlayScreen playScreen, Exchangeable messenger) {
 		this.name = name;
 		this.screen = playScreen;
+		this.messenger = messenger;
 	}
 	
 	/**
@@ -15,19 +23,30 @@ public class ComputerPlayer implements Player {
 	 */
 	@Override
 	public void updateState(CortexState state) {
-		
+		int chosenNumber = state.getChosenNumber();
+		if (chosenNumber != -1) {
+			for (Map.Entry<Integer, Integer> entry : state.getCoordinateNumberMap().entrySet()) {
+				if (entry.getValue() == -1) {
+					int coordinate = entry.getKey();
+					placeNumber(name, coordinate, chosenNumber);
+					return;
+				}
+			}
+		} else {
+			int nextNumber = state.getAvailableNumbers().get(0);
+			chooseNumber(name, nextNumber);
+			return;
+		}
 	}
 	
 	@Override
 	public void chooseNumber(String player, int nextNumber) {
-		// TODO Auto-generated method stub
-		
+		messenger.chooseNumber(name, nextNumber);
 	}
 
 	@Override
 	public void placeNumber(String player, int coordinate, int number) {
-		// TODO Auto-generated method stub
-		
+		messenger.placeNumber(name, coordinate, number);
 	}
 
 	@Override
