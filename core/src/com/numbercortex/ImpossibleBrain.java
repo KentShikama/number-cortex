7974156de4d187cc011d2a19a531481eff3c19d1
theworldbright <sushionigiris@gmail.com>
2014-06-29 23:52:41 +0900

    Implement ImpossibleBrain

---
+++ core/src/com/numbercortex/ImpossibleBrain.java
@@ -0,0 +1,34 @@
+package com.numbercortex;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class ImpossibleBrain implements Brain {
+	
+	@Override
+	public int calculateCoordinate(CortexState state) {
+		int chosenNumber = state.getChosenNumber();
+		Map<Integer, Integer> coordinateNumberMap = state.getCoordinateNumberMap();
+		ArrayList<Integer> openCoordinates = BrainUtilities.getOpenCoordinates(coordinateNumberMap);
+		
+		int chosenCoordinate = BrainUtilities.assignWinningCoordinateIfExistent(chosenNumber, coordinateNumberMap, openCoordinates);
+		if (chosenCoordinate == -1) {
+			// Check if there is a safe number that matches the chosenCoordinate instead of a random coordinate
+			chosenCoordinate = BrainUtilities.assignRandomNumberFromList(openCoordinates);
+		}
+		return chosenCoordinate;
+	}
+
+	@Override
+	public int calculateNextNumber(CortexState state) {
+		Map<Integer, Integer> coordinateNumberMap = state.getCoordinateNumberMap();
+		ArrayList<Integer> availableNumbers = state.getAvailableNumbers();
+
+		int nextNumber = BrainUtilities.assignSafeNumberIfExistent(coordinateNumberMap, availableNumbers);
+		if (nextNumber == -1) {
+			nextNumber = BrainUtilities.assignRandomNumberFromList(availableNumbers);
+		}
+		return nextNumber;
+	}
+
+}
