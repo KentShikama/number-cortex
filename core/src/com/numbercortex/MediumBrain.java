28d7f7fdd2a181664e0ac46ffbf247152c575dd6
theworldbright <sushionigiris@gmail.com>
2014-06-29 19:04:54 +0900

    Implement medium difficulty AI

---
+++ core/src/com/numbercortex/MediumBrain.java
@@ -0,0 +1,51 @@
+package com.numbercortex;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class MediumBrain implements Brain {
+	
+	private CortexPreferences preferences = CortexPreferences.getInstance();
+
+	@Override
+	public int calculateCoordinate(CortexState state) {
+		ArrayList<Integer> openCoordinates = new ArrayList<Integer>();
+		for (Map.Entry<Integer, Integer> entry : state.getCoordinateNumberMap().entrySet()) {
+			if (entry.getValue() == -1) {
+				int openCoordinate = entry.getKey();
+				openCoordinates.add(openCoordinate);
+			}
+		}
+		int chosenCoordinate = assignWinningCoordinateIfExistent(state, openCoordinates);
+		if (chosenCoordinate == -1) {
+			int chosenCoordinatePosition = (int) (Math.random() * openCoordinates.size());
+			chosenCoordinate = openCoordinates.get(chosenCoordinatePosition);
+		}
+		return chosenCoordinate;
+	}
+
+	private int assignWinningCoordinateIfExistent(CortexState state, ArrayList<Integer> openCoordinates) {
+		int chosenNumber = state.getChosenNumber();
+		Map<Integer, Integer> coordinateNumberMap = state.getCoordinateNumberMap();
+		int chosenCoordinate = -1;
+		for (Integer openCoordinate : openCoordinates) {
+			coordinateNumberMap.put(openCoordinate, chosenNumber);
+			int[] winningCoordinates = WinHandler.handleWinningBoard(coordinateNumberMap, preferences);
+			coordinateNumberMap.replace(openCoordinate, -1);
+			if (winningCoordinates != null) {
+				chosenCoordinate = openCoordinate;
+				break;
+			}
+		}
+		return chosenCoordinate;
+	}
+
+	@Override
+	public int calculateNextNumber(CortexState state) {
+		ArrayList<Integer> availableNumbers = state.getAvailableNumbers();
+		int nextNumberPosition = (int) (Math.random() * availableNumbers.size());
+		int nextNumber = availableNumbers.get(nextNumberPosition);
+		return nextNumber;
+	}
+
+}
