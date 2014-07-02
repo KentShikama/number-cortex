d765c6989f78d68fa6a682054bb72a546dc1bd03
theworldbright <sushionigiris@gmail.com>
2014-07-02 20:11:28 +0900

    Update win handler to account for new game settings

--- core/src/com/numbercortex/WinHandler.java
+++ core/src/com/numbercortex/WinHandler.java
@@ -9,26 +9,26 @@ public class WinHandler {
 	public static int[] handleWinningBoard(Map<Integer, Integer> coordinateNumberMap, GameSettings settings) {
 		int[][] translatedCoordinateNumberMap = translateCoordinates(coordinateNumberMap);
 		int[] winningSet;
-		winningSet = checkAndHandleHorizontals(translatedCoordinateNumberMap);
+		winningSet = checkAndHandleHorizontals(translatedCoordinateNumberMap, settings);
 		if (winningSet != null) {
 			return winningSet;
 		}
-		winningSet = checkAndHandleVerticals(translatedCoordinateNumberMap);
+		winningSet = checkAndHandleVerticals(translatedCoordinateNumberMap, settings);
 		if (winningSet != null) {
 			return winningSet;
 		}
 		if (settings.isDiagonals()) {
-			winningSet = checkAndHandleLeftDiagonal(translatedCoordinateNumberMap);
+			winningSet = checkAndHandleLeftDiagonal(translatedCoordinateNumberMap, settings);
 			if (winningSet != null) {
 				return winningSet;
 			}
-			winningSet = checkAndHandleRightDiagonal(translatedCoordinateNumberMap);
+			winningSet = checkAndHandleRightDiagonal(translatedCoordinateNumberMap, settings);
 			if (winningSet != null) {
 				return winningSet;
 			}
 		}
 		if (settings.isFourSquare()) {
-			winningSet = checkAndHandleFourSquare(translatedCoordinateNumberMap);
+			winningSet = checkAndHandleFourSquare(translatedCoordinateNumberMap, settings);
 			if (winningSet != null) {
 				return winningSet;
 			}
@@ -44,52 +44,52 @@ public class WinHandler {
 		return translatedCoordinateNumberMap;
 	}
 
-	private static int[] checkAndHandleHorizontals(int[][] translatedCoordinateNumberMap) {
+	private static int[] checkAndHandleHorizontals(int[][] translatedCoordinateNumberMap, GameSettings settings) {
 		int numberOfRows = translatedCoordinateNumberMap.length;
 		for (int row = 0; row < numberOfRows; row++) {
 			int[] set = translatedCoordinateNumberMap[row];
-			if (isBingo(set)) {
+			if (isBingo(set, settings)) {
 				return set;
 			}
 		}
 		return null;
 	}
-	private static int[] checkAndHandleVerticals(int[][] translatedCoordinateNumberMap) {
+	private static int[] checkAndHandleVerticals(int[][] translatedCoordinateNumberMap, GameSettings settings) {
 		int numberOfRows = translatedCoordinateNumberMap.length;
 		for (int column = 0; column < numberOfRows; column++) {
 			int[] set = new int[numberOfRows];
 			for (int i = 0; i < numberOfRows; i++) {
 				set[i] = translatedCoordinateNumberMap[i][column];
 			}
-			if (isBingo(set)) {
+			if (isBingo(set, settings)) {
 				return set;
 			}
 		}
 		return null;
 	}
-	private static int[] checkAndHandleLeftDiagonal(int[][] translatedCoordinateNumberMap) {
+	private static int[] checkAndHandleLeftDiagonal(int[][] translatedCoordinateNumberMap, GameSettings settings) {
 		int numberOfRows = translatedCoordinateNumberMap.length;
 		int[] leftDiagonalSet = new int[numberOfRows];
 		for (int i = 0; i < numberOfRows; i++) {
 			leftDiagonalSet[i] = translatedCoordinateNumberMap[i][i];
 		}
-		if (isBingo(leftDiagonalSet)) {
+		if (isBingo(leftDiagonalSet, settings)) {
 			return leftDiagonalSet;
 		}
 		return null;
 	}
-	private static int[] checkAndHandleRightDiagonal(int[][] translatedCoordinateNumberMap) {
+	private static int[] checkAndHandleRightDiagonal(int[][] translatedCoordinateNumberMap, GameSettings settings) {
 		int numberOfRows = translatedCoordinateNumberMap.length;
 		int[] rightDiagonalSet = new int[numberOfRows];
 		for (int i = 0; i < numberOfRows; i++) {
 			rightDiagonalSet[i] = translatedCoordinateNumberMap[numberOfRows - 1 - i][i];
 		}
-		if (isBingo(rightDiagonalSet)) {
+		if (isBingo(rightDiagonalSet, settings)) {
 			return rightDiagonalSet;
 		}
 		return null;
 	}
-	private static int[] checkAndHandleFourSquare(int[][] translatedCoordinateNumberMap) {
+	private static int[] checkAndHandleFourSquare(int[][] translatedCoordinateNumberMap, GameSettings settings) {
 		int numberOfRows = translatedCoordinateNumberMap.length;
 		int[] set = new int[4];
 		for (int row = 0; row < numberOfRows - 1; row++) {
@@ -98,7 +98,7 @@ public class WinHandler {
 				set[1] = translatedCoordinateNumberMap[row + 1][column];
 				set[2] = translatedCoordinateNumberMap[row][column + 1];
 				set[3] = translatedCoordinateNumberMap[row + 1][column + 1];
-				if (isBingo(set)) {
+				if (isBingo(set, settings)) {
 					return set;
 				}
 			}
@@ -106,22 +106,22 @@ public class WinHandler {
 		return null;
 	}
 
-	private static boolean isBingo(int[] set) {
+	private static boolean isBingo(int[] set, GameSettings settings) {
 		for (int i = 0; i < set.length; i++) {
 			if (set[i] == -1) {
 				return false;
 			}
 		}
-		if (isValidEvenOdd(set)) {
+		if (settings.isEvenOdd() && isValidEvenOdd(set)) {
 			return true;
 		}
-		if (isValidSingleDouble(set)) {
+		if (settings.isSingleDouble() && isValidSingleDouble(set)) {
 			return true;
 		}
-		if (isValidPrimeComposite(set)) {
+		if (settings.isPrimeComposite() && isValidPrimeComposite(set)) {
 			return true;
 		}
-		if (isValidMiddleExtreme(set)) {
+		if (settings.isMiddleExtreme() && isValidMiddleExtreme(set)) {
 			return true;
 		}
 		return false;
