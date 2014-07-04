fd1a6ba4788916fae2551e14296d12d4b8efa7c6
worldbright <sushionigiris@gmail.com>
2014-07-04 23:09:09 +0900

    Refactor levels screen

--- core/src/com/numbercortex/LevelsScreen.java
+++ core/src/com/numbercortex/LevelsScreen.java
@@ -26,7 +26,38 @@ public class LevelsScreen implements Screen {
 	private Game game;
 	private Stage stage;
 	private ArrayList<TextButton> textButtons = buildTextButtons(18);
+	private ArrayList<TextButton> buildTextButtons(int numberOfLevels) {
+		ArrayList<TextButton> textButtons = new ArrayList<TextButton>();
+		for (int i = 0; i < numberOfLevels; i++) {
+			textButtons.add(buildTextButton(i + 1));
+		}
+		return textButtons;
+	}
+	private TextButton buildTextButton(int level) {
+		return new TextButton(String.valueOf(level), levelButtonStyle);
+	}
+	
 	private ArrayList<ClickListener> listeners = buildClicklisteners(18);
+	private ArrayList<ClickListener> buildClicklisteners(int numberOfLevels) {
+		ArrayList<ClickListener> listeners = new ArrayList<ClickListener>();
+		for (int i = 0; i < numberOfLevels; i++) {
+			listeners.add(buildClickListener(i + 1));
+		}
+		return listeners;
+	}
+	private ClickListener buildClickListener(final int level) {
+		ClickListener listener = new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				TextButton button = (TextButton) event.getListenerActor();
+				if (!button.isDisabled()) {
+					ScreenTracker.level = level;
+					game.setScreen(ScreenTracker.playScreen);
+				}
+			}
+		};
+		return listener;
+	}
 	
 	private static final String TITLE = "level_selector";
 	private static final String BOARD_SIZE_3_LABEL = "3x3";
@@ -70,38 +101,6 @@ public class LevelsScreen implements Screen {
 		return levelButton;
 	}
 	
-	private ArrayList<ClickListener> buildClicklisteners(int numberOfLevels) {
-		ArrayList<ClickListener> listeners = new ArrayList<ClickListener>();
-		for (int i = 0; i < numberOfLevels; i++) {
-			listeners.add(buildClickListener(i + 1));
-		}
-		return listeners;
-	}
-	private ClickListener buildClickListener(final int level) {
-		ClickListener listener = new ClickListener() {
-			@Override
-			public void clicked(InputEvent event, float x, float y) {
-				TextButton button = (TextButton) event.getListenerActor();
-				if (!button.isDisabled()) {
-					ScreenTracker.level = level;
-					game.setScreen(ScreenTracker.playScreen);
-				}
-			}
-		};
-		return listener;
-	}
-	
-	private ArrayList<TextButton> buildTextButtons(int numberOfLevels) {
-		ArrayList<TextButton> textButtons = new ArrayList<TextButton>();
-		for (int i = 0; i < numberOfLevels; i++) {
-			textButtons.add(buildTextButton(i + 1));
-		}
-		return textButtons;
-	}
-	private TextButton buildTextButton(int level) {
-		return new TextButton(String.valueOf(level), levelButtonStyle);
-	}
-	
 	@Override
 	public void show() {
 		stage.clear();
@@ -120,64 +119,46 @@ public class LevelsScreen implements Screen {
 		table.add(boardSize3Label).left().padBottom(20).padTop(20).colspan(2);
 		table.row();
 		
-		TextButton level1 = buildEnabledLevel(1);
-		TextButton level2 = buildEnabledLevel(2);
-		TextButton level3 = buildEnabledLevel(3);
-
-		table.add(level1).size(100, 100).pad(20).right();
-		table.add(level2).size(100, 100).pad(20);
-		table.add(level3).size(100, 100).pad(20).left();
-		table.row();
-		
-		TextButton level4 = buildEnabledLevel(4);
-		TextButton level5 = buildEnabledLevel(5);
-		TextButton level6 = buildEnabledLevel(6);
-
-		table.add(level4).size(100, 100).pad(20).right();
-		table.add(level5).size(100, 100).pad(20);
-		table.add(level6).size(100, 100).pad(20).left();
-		table.row();
-		
-		TextButton level7 = buildEnabledLevel(7);
-		TextButton level8 = buildEnabledLevel(8);
-		TextButton level9 = buildEnabledLevel(9);
-
-		table.add(level7).size(100, 100).pad(20).right();
-		table.add(level8).size(100, 100).pad(20);
-		table.add(level9).size(100, 100).pad(20).left();
-		table.row();
-	
+		for (int i = 0; i < 9; i++) {
+			int level = i + 1;
+			TextButton levelButton;
+			if (level <= preferences.getCurrentLevel()) {
+				levelButton = buildEnabledLevel(level);
+			} else {
+				levelButton = buildDisabledLevel(level);
+			}
+			if (level % 3 == 1) {
+				table.add(levelButton).size(100, 100).pad(20).right();
+			} else if (level % 3 == 2) {
+				table.add(levelButton).size(100, 100).pad(20);
+			} else {
+				table.add(levelButton).size(100, 100).pad(20).left();
+				table.row();
+			}
+		}
+ 
 		Drawable boardSize4LabelDrawable = skin.getDrawable(BOARD_SIZE_4_LABEL);
 		Image boardSize4Label = new Image(boardSize4LabelDrawable);
 		table.add(boardSize4Label).left().padBottom(20).padTop(20).colspan(2);
 		table.row();
 		
-		TextButton level10 = buildEnabledLevel(10);
-		TextButton level11 = buildEnabledLevel(11);
-		TextButton level12 = buildEnabledLevel(12);
-
-		table.add(level10).size(100, 100).pad(20).right();
-		table.add(level11).size(100, 100).pad(20);
-		table.add(level12).size(100, 100).pad(20).left();
-		table.row();
-		
-		TextButton level13 = buildEnabledLevel(13);
-		TextButton level14 = buildEnabledLevel(14);
-		TextButton level15 = buildEnabledLevel(15);
-
-		table.add(level13).size(100, 100).pad(20).right();
-		table.add(level14).size(100, 100).pad(20);
-		table.add(level15).size(100, 100).pad(20).left();
-		table.row();
-		
-		TextButton level16 = buildEnabledLevel(16);
-		TextButton level17 = buildEnabledLevel(17);
-		TextButton level18 = buildDisabledLevel(18);
-		
-		table.add(level16).size(100, 100).pad(20).right();
-		table.add(level17).size(100, 100).pad(20);
-		table.add(level18).size(100, 100).pad(20).left();
-		table.row();
+		for (int i = 9; i < 18; i++) {
+			int level = i + 1;
+			TextButton levelButton;
+			if (level <= preferences.getCurrentLevel()) {
+				levelButton = buildEnabledLevel(level);
+			} else {
+				levelButton = buildDisabledLevel(level);
+			}
+			if (level % 3 == 1) {
+				table.add(levelButton).size(100, 100).pad(20).right();
+			} else if (level % 3 == 2) {
+				table.add(levelButton).size(100, 100).pad(20);
+			} else {
+				table.add(levelButton).size(100, 100).pad(20).left();
+				table.row();
+			}
+		}
 		
 		ScrollPane pane = new ScrollPane(table);
 		pane.setBounds(0, 0, Launch.SCREEN_WIDTH, Launch.SCREEN_HEIGHT);
