12fc20a4c9e442630f0a8cac5179a120c04a9545
theworldbright <sushionigiris@gmail.com>
2014-07-19 20:59:03 +0900

    Ensure click sound only appears if control is clickable

--- core/src/com/numbercortex/view/ClickListenerWithSound.java
+++ core/src/com/numbercortex/view/ClickListenerWithSound.java
@@ -1,15 +1,37 @@
 package com.numbercortex.view;
 
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.numbercortex.Persistence;
+import com.numbercortex.view.SettingsScreen.GroupState;
+import com.numbercortex.view.SettingsScreen.SettingGroup;
 
 public class ClickListenerWithSound extends ClickListener {
 	public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
 		super.touchDown(event, x, y, pointer, button);
-		if (Persistence.getInstance().isSound()) {
-			Sound.click();
+		Actor targetActor = event.getTarget();
+		SettingGroup settingGroup = getSettingsGroup(targetActor);
+		if (settingGroup != null) {
+			GroupState state = settingGroup.groupState;
+			if (Persistence.getInstance().isSound() && state == GroupState.CLICKABLE) {
+				Sound.click();
+			}
+		} else {
+			if (Persistence.getInstance().isSound()) {
+				Sound.click();
+			}
 		}
 		return true;
+	}
+	
+	private SettingGroup getSettingsGroup(Actor actor) {
+		if (actor == null) {
+			return null;
+		} else if (actor instanceof SettingGroup) {
+			return (SettingGroup) actor;
+		} else {
+			return getSettingsGroup(actor.getParent());
+		}
 	}
 }
