21588793995351bca57b9469c95a86fd550e58d4
theworldbright <sushionigiris@gmail.com>
2014-08-02 18:43:46 +0900

    Implement iOS Chartboost more apps

--- ios/src/com/numbercortex/IOSLauncher.java
+++ ios/src/com/numbercortex/IOSLauncher.java
@@ -2,30 +2,30 @@ package com.numbercortex;
 
 import org.robovm.apple.foundation.NSAutoreleasePool;
 import org.robovm.apple.uikit.UIApplication;
+import org.robovm.bindings.chartboost.Chartboost;
+import org.robovm.bindings.chartboost.ChartboostDelegate;
 
-import chartboost.ChartBoostListener;
+import chartboost.AppleChartboost;
 import chartboost.CrossPlatformChartboost;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
 import com.numbercortex.view.Launch;
 
 public class IOSLauncher extends IOSApplication.Delegate {
+    private Chartboost chartboost;
+
     @Override
     protected IOSApplication createApplication() {
         IOSApplicationConfiguration config = new IOSApplicationConfiguration();
         config.orientationPortrait = true;
         config.orientationLandscape = false;
-        CrossPlatformChartboost chartboost = new CrossPlatformChartboost() {
-            @Override
-            public void showMoreApps() {}
-            @Override
-            public Object getChartBoostDelegate() {
-                return null;
-            }
-            @Override
-            public void setListener(ChartBoostListener listener) {}        
-        };
-        return new IOSApplication(new Launch(chartboost), config);
+        chartboost = Chartboost.sharedChartboost();
+        chartboost.setAppId("53dc9c641873da4ec7b5a2b8");
+        chartboost.setAppSignature("ea716c2371dffac4ca1425817ef73df8ea17485b");
+        CrossPlatformChartboost crossPlatformChartboost = new AppleChartboost(chartboost);
+        chartboost.setDelegate((ChartboostDelegate) crossPlatformChartboost.getChartBoostDelegate());
+        chartboost.startSession();
+        return new IOSApplication(new Launch(crossPlatformChartboost), config);
     }
 
     public static void main(String[] argv) {
