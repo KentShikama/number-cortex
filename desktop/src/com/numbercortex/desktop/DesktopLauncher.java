3ce4e0e7bc06c67916fe021862764570183a48a3
theworldbright <sushionigiris@gmail.com>
2014-08-03 00:41:39 +0900

    Initial commit for Eclipse Luna

---
+++ desktop/src/com/numbercortex/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.numbercortex.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.numbercortex.Launch;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Launch(), config);
+	}
+}
