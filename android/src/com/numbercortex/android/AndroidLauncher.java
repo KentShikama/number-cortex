f6a150470a84ceb91c39904d9a681f8dac066c88
theworldbright <sushionigiris@gmail.com>
2014-08-04 18:32:18 +0900

    Implement basic Facebook login integration

--- android/src/com/numbercortex/android/AndroidLauncher.java
+++ android/src/com/numbercortex/android/AndroidLauncher.java
@@ -8,6 +8,11 @@ import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.numbercortex.view.Launch;
 import com.chartboost.sdk.CBPreferences;
 import com.chartboost.sdk.Chartboost;
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.model.GraphUser;
 
 public class AndroidLauncher extends AndroidApplication {
 
@@ -24,6 +29,30 @@ public class AndroidLauncher extends AndroidApplication {
         this.cb.onCreate(this, appId, appSignature, ((AndroidChartboost) crossPlatformChartboost).getChartBoostDelegate());
         AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
         initialize(new Launch(crossPlatformChartboost), config);
+        Session.openActiveSession(this, true, new Session.StatusCallback() {
+            // callback when session changes state
+            @Override
+            public void call(Session session, SessionState state, Exception exception) {
+              if (session.isOpened()) {
+
+                // make request to the /me API
+                Request.newMeRequest(session, new Request.GraphUserCallback() {
+
+                  // callback after Graph API response with user object
+                  @Override
+                  public void onCompleted(GraphUser user, Response response) {
+                    if (user != null) {
+                        System.out.println("Connected");
+                        System.out.println("Connected");
+                        System.out.println("Connected");
+                        System.out.println("Connected");
+                        System.out.println("Connected");
+                    }
+                  }
+                }).executeAsync();
+              }
+            }
+          });
     }
 
     @Override
