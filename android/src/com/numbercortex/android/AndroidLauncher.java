e771adf6531c356b8f5ac0e00f362a7cd261440d
theworldbright <sushionigiris@gmail.com>
2014-08-05 01:32:54 +0900

    Implement Facebook sharing
    The sharing option still needs
    to be activated only by button click.

--- android/src/com/numbercortex/android/AndroidLauncher.java
+++ android/src/com/numbercortex/android/AndroidLauncher.java
@@ -12,12 +12,25 @@ import com.chartboost.sdk.Chartboost;
 import com.facebook.Request;
 import com.facebook.Response;
 import com.facebook.Session;
+import com.facebook.Session.StatusCallback;
 import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
+import com.facebook.model.GraphObject;
 import com.facebook.model.GraphUser;
+import com.facebook.model.OpenGraphAction;
+import com.facebook.model.OpenGraphObject;
+import com.facebook.widget.FacebookDialog;
 
 public class AndroidLauncher extends AndroidApplication {
 
     private Chartboost cb;
+    private UiLifecycleHelper uiHelper;
+    // private StatusCallback statusCallback = new Session.StatusCallback() {
+    // @Override
+    // public void call(Session session, SessionState state, Exception exception) {
+    //
+    // }
+    // };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,34 +43,57 @@ public class AndroidLauncher extends AndroidApplication {
         this.cb.onCreate(this, appId, appSignature, ((AndroidChartboost) crossPlatformChartboost).getChartBoostDelegate());
         AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
         initialize(new Launch(crossPlatformChartboost), config);
-        Session.openActiveSession(this, true, new Session.StatusCallback() {
-            // callback when session changes state
-            @Override
-            public void call(Session session, SessionState state, Exception exception) {
-              if (session.isOpened()) {
 
-                // make request to the /me API
-                Request.newMeRequest(session, new Request.GraphUserCallback() {
+        uiHelper = new UiLifecycleHelper(this, null);
+        uiHelper.onCreate(savedInstanceState);
 
-                  // callback after Graph API response with user object
-                  @Override
-                  public void onCompleted(GraphUser user, Response response) {
-                    if (user != null) {
-                        System.out.println("Connected");
-                    }
-                  }
-                }).executeAsync();
-              }
-            }
-          });
+        if (FacebookDialog.canPresentShareDialog(getApplicationContext(), FacebookDialog.ShareDialogFeature.SHARE_DIALOG)) {
+            FacebookDialog shareDialog = new FacebookDialog.ShareDialogBuilder(this)
+            .setLink("http://www.numbercortex.com")
+            .setPicture("http://www.numbercortex.com/images/number_cortex_mobile_banner.jpg")
+            .setDescription("Interested in puzzle games? Challenge yourself with the new two player board game, Number Cortex.")
+            .setCaption(" ").setName("Level 8 Cleared!").build();
+            uiHelper.trackPendingDialogCall(shareDialog.present());
+        } else {
+            // Publish the post using the Feed Dialog
+        }
     }
-    
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
+        uiHelper.onActivityResult(requestCode, resultCode, data, new FacebookDialog.Callback() {
+            @Override
+            public void onError(FacebookDialog.PendingCall pendingCall, Exception error, Bundle data) {
+                System.out.println(String.format("Error: %s", error.toString()));
+            }
+            @Override
+            public void onComplete(FacebookDialog.PendingCall pendingCall, Bundle data) {
+                System.out.println("Success!");
+            }
+        });
     }
-
+    @Override
+    protected void onResume() {
+        super.onResume();
+        uiHelper.onResume();
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        uiHelper.onSaveInstanceState(outState);
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        uiHelper.onPause();
+    }
+    @Override
+    protected void onDestroy() {
+        this.cb.onDestroy(this);
+        super.onDestroy();
+        uiHelper.onDestroy();
+    }
     @Override
     protected void onStart() {
         super.onStart();
@@ -69,17 +105,11 @@ public class AndroidLauncher extends AndroidApplication {
         this.cb.onStop(this);
     }
     @Override
-    protected void onDestroy() {
-        this.cb.onDestroy(this);
-        super.onDestroy();
-    }
-    @Override
     public void onBackPressed() {
         if (this.cb.onBackPressed()) {
             return;
+        } else {
+            super.onBackPressed();
         }
-        else {
-            super.onBackPressed();            
-        }
-    }   
+    }
 }
